13699d70117115f2b7566de328e93d90164fc7fa
Drukpa77 <83352767+Drukpa77@users.noreply.github.com>
2024-07-10 02:06:17 +1000

    Introducing bcrypt
    Hashing password for the database to secure the password of the client

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,40 +1,43 @@
-const User = require('../models/user')
+const User = require("../models/user");
+const {hashPassword, comparePassword} = require('../helpers/auth')
 
 const test = (req, res) => {
   res.json("test is working");
 };
 
-const registerUser = async(req, res) => {
+const registerUser = async (req, res) => {
   try {
-    const { name, email, password } = res.body;
+    const { name, email, password } = req.body;
     // Check if name was entered
-    if(!name){
-        return res.json({
-            error:"Name is Requried"
-        })
-    };
+    if (!name) {
+      return res.json({
+        error: "Name is Requried",
+      });
+    }
     //check if password is good
-    if(!passowrd || password.length < 6){
-        return res.json({
-            error:"Password is required and should be 6 characters long"
-        })
-
-    };
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and should be 6 characters long",
+      });
+    }
     //Check Email
-    const exist = await User.findOne({email});
-    if(exist) {
-        return res.json({
-            error:"Email is Taken already"
-        })
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is Taken already",
+      });
     }
 
-    const user = await User.create({
-        name,email,password
-    })
-    return res.json(user)
-  } catch (error) {
-    console.log(error)
+    const hashedPassword = await hashPassword(password)
 
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
   }
 };
 
