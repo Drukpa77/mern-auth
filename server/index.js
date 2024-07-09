3102911877550d3e1cf4328f6b674a132a1127a3
Drukpa77 <83352767+Drukpa77@users.noreply.github.com>
2024-07-10 01:02:48 +1000

    changes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const dotenv = require('dotenv').config()
-const cors = require('cors')
-const {mongoose} = require('mongoose')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const { mongoose } = require("mongoose");
 const app = express();
 
 //database connection
-mongoose.connect(process.env.MONGO_URL)
-.then(()=> console.log('Database connected'))
-.catch(()=> console.log('Databse Not connected', err))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Databse Not connected", err));
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 
-app.use('/', require('./routes/authRoutes'))
+app.use("/", require("./routes/authRoutes"));
 
 const port = 8000;
-app.listen(port, ()=> console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
